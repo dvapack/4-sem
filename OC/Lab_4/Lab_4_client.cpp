f882304cd94d68469114a899bcb4eac6f8de5236
dvapack <67041431+dvapack@users.noreply.github.com>
2024-03-30 23:00:05 +0400

    оно живое, но надо наводить красоту

---
+++ OC/Lab_4/Lab_4_client.cpp
@@ -0,0 +1,84 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#pragma warning(disable:4996)
+
+/*
+Вариант 25-26.
+С процесса-сервера запускается n процессов клиентов. На
+клиенте вводится текст, который затем отсылается на сервер. На
+сервере происходит анализ: в полученном сообщении подсчитывается частота появления каждого символа. Далее информация отсылается на клиент.
+*/
+
+#define SECOND 1000
+
+struct Map
+{
+	std::vector <int> nums;
+	std::vector <char> symbols;
+};
+
+
+
+
+
+int main(int argc, char* argv[]) {
+	//Загрузка библиотеки 
+	WSAData wsaData; //создаем структуру для загрузки
+	WORD DLLVersion = MAKEWORD(2, 1); // Версия библиотеки winsock
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { // проверка подключения
+		std::cerr << "Error: failed to link library.\n";
+		return 1;
+	}
+	//Заполняем информацию об адресе сокета
+	SOCKADDR_IN addr;
+	int sizeOfAddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+	SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
+	//проверка на подключение к серверу
+	if (connect(Connection, (SOCKADDR*)&addr, sizeof(addr))) {
+		std::cerr << "Error: failed connect to the server.\n";
+		return 1;
+	}
+	std::cout << "Client Connected to server!\n";
+	std::string input;
+	std::cout << "Enter string: ";
+	std::getline(std::cin, input);
+	int len = input.length();
+	send(Connection, (char*)&len, sizeof(int), NULL);
+	char* msg;
+	msg = new char[len + 1];
+	for (int i = 0; i < len; ++i)
+		msg[i] = input[i];
+	msg[len] = '\0';
+	send(Connection, msg, len, NULL);
+	Map result;
+	int res_len;
+	recv(Connection, (char*)&res_len, sizeof(int), NULL);
+	char* symbols = new char[res_len];
+	recv(Connection, symbols, res_len, NULL);
+	res_len *= sizeof(int);
+	char* res = new char[res_len];
+	recv(Connection, res, res_len, NULL);
+	int* nums = (int*)res;
+	for (int i = 0; i < res_len/sizeof(int); ++i)
+	{
+		std::cout << "Symbol: " << symbols[i] <<
+			" - " << nums[i] << std::endl;
+	}
+	std::string i;
+	std::getline(std::cin, i);
+	std::cout << "Server has been stopped\n";
+	if (closesocket(Connection) == SOCKET_ERROR)
+		std::cerr << "Failed to terminate connection.\n Error code: " << WSAGetLastError();
+	WSACleanup();
+	std::cout << "Correct exit done\n";
+	delete[] msg;
+	delete[] res;
+	delete[] symbols;
+	return 0;
+}
---
+++ OC/Lab_4/Lab_4_server.cpp
@@ -0,0 +1,152 @@
+﻿#pragma comment(lib, "ws2_32.lib")
+#include <winsock2.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#pragma warning(disable: 4996)
+
+/*
+Вариант 25-26.
+С процесса-сервера запускается n процессов клиентов. На
+клиенте вводится текст, который затем отсылается на сервер. На
+сервере происходит анализ: в полученном сообщении подсчитывается частота появления каждого символа. Далее информация отсылается на клиент.
+*/
+
+SOCKET Connections[100];
+int counter = 0;
+
+struct Map 
+{
+	std::vector <int> nums;
+	std::vector <char> symbols;
+};
+
+void start_client_session()
+{
+	const char* navigation_command = "cd C:\\Users\\mserg\\source\\repos\\OC_Lab_2_client\\x64\\Debug";
+	const char* run_command = "start C:\\Users\\mserg\\source\\repos\\OC_Lab_2_client\\x64\\Debug\\OC_Lab_2_client.exe";
+	// проверка на случай непредвиденных обстоятельств
+	int navigation_failure = system(navigation_command);
+	if (navigation_failure)
+	{
+		std::cout << "Failure in system navigation!" << '\n';
+		exit(1);
+	}
+	// запускаем собранную программу у клиента
+	system(run_command);
+}
+
+void string_check(int len, char* msg, Map& result)
+{
+	std::string mes(msg);
+	int i = 0;
+	while(mes.length() > 0)
+	{
+		result.symbols.push_back(mes[0]);
+		result.nums.push_back(0);
+		char symbol = mes[0];
+		for (int j = 0; j < mes.length(); ++j)
+		{
+			if (symbol == mes[j])
+			{
+				++result.nums[i];
+				mes.erase(mes.begin()+j);
+				--j;
+			}
+		}
+		++i;
+	}
+}
+
+bool check_input(std::string& input)
+{
+	std::getline(std::cin, input);
+	for (int i = 0; i < input.length(); ++i)
+		if (!isdigit(input[i]))
+			return false;
+	int n = stoi(input);
+	if (n >= 1 && n <= 100)
+		return true;
+	return false;
+}
+
+int main(int argc, char* argv[])
+{
+	int n;
+	std::string input;
+	std::cout << "Enter a positive num 1 <= n <= 100: ";
+	while (!check_input(input))
+		std::cout << "Error! Enter a positive num 1 <= n <= 100\n";
+	n = stoi(input);
+	//Загрузка библиотеки 
+	WSAData wsaData; //создаем структуру для загрузки
+	WORD DLLVersion = MAKEWORD(2, 1); // Версия библиотеки winsock
+	if (WSAStartup(DLLVersion, &wsaData) != 0) { // проверка подключения
+		std::cerr << "Error: failed to link library.\n";
+		return 1;
+	}
+	//Заполняем информацию об адресе сокета
+	SOCKADDR_IN addr;
+	int sizeOfAddr = sizeof(addr);
+	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	addr.sin_port = htons(1111);
+	addr.sin_family = AF_INET;
+	SOCKET sListen = socket(AF_INET, SOCK_STREAM, NULL); //сокет для прослушивания порта
+	if (bind(sListen, (SOCKADDR*)&addr, sizeOfAddr) == SOCKET_ERROR) {
+		//привязка адреса сокету
+		printf("Error bind %d\n", WSAGetLastError());
+		closesocket(sListen);
+		WSACleanup();
+		return 1;
+	} //подключение прослушивания с максимальной очередью
+	if (listen(sListen, SOMAXCONN) == SOCKET_ERROR) {
+		std::cout << "Listen failed;\n";
+		closesocket(sListen);
+		WSACleanup();
+		return 1;
+	}
+	SOCKET newConnection;
+	for (int i = 0; i < n; ++i)
+	{
+		start_client_session();
+		newConnection = accept(sListen, (SOCKADDR*)&addr, &sizeOfAddr);
+		if (!newConnection)
+		{
+			std::cout << "Error in connect!\n";
+			if (closesocket(sListen) == SOCKET_ERROR)
+				std::cerr << "Failed to terminate connection.\n Error code: " << WSAGetLastError();
+			WSACleanup();
+			return 1;
+		}
+		else
+		{
+			std::cout << "Client Connected!\n";
+			int len;
+			recv(newConnection, (char*)&len, sizeof(int), NULL);
+			char* message;
+			message = new char[len + 1];
+			message[len] = '\0';
+			recv(newConnection, message, len, NULL);
+			Map result;
+			string_check(len, message, result);
+			int nums_res_len = result.nums.size();
+			int* nums = new int[nums_res_len];
+			char* symbols = new char[nums_res_len];
+			for (int i = 0; i < nums_res_len; ++i)
+			{
+				nums[i] = result.nums[i];
+				symbols[i] = result.symbols[i];
+			}
+			char* nums_res = (char*)nums;
+			send(newConnection, (char*)&nums_res_len, sizeof(int), NULL);
+			send(newConnection, symbols, nums_res_len, NULL);
+			send(newConnection, nums_res, sizeof(int)*nums_res_len, NULL);
+			delete[] nums;
+			delete[] symbols;
+		}
+		Connections[i] = newConnection;
+		++counter;
+	}
+	return 0;
+}
